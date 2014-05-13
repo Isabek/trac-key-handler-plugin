55a844a11784039a6005afe57b943021b743749a
Isabek Tashiev <isabek2309@gmail.com>
2014-05-13 13:42:23 -0400

    chan process request

--- keyhandler/matcher.py
+++ keyhandler/matcher.py
@@ -1,6 +1,7 @@
 import re
 from trac.core import Component, implements
 from trac.web import IRequestHandler
+from trac.web.chrome import add_script
 
 
 class KeyHandlerModule(Component):
@@ -8,15 +9,19 @@ class KeyHandlerModule(Component):
 
     def match_request(self, req):
         if re.match(r'/newticket', req.path_info):
-            return True
+            add_script(req, 'keyhandler/htdocs/js/main.js')
+        return False
 
     def process_request(self, req):
-        return None
+        req.send_response(200)
+        content = 'Hello world!'
+        req.send_header('Content-Type', 'text/plain')
+        req.send_header('Content-length', str(len(content)))
+        req.end_headers()
+        req.write(content)
 
     def get_templates_dir(self):
         pass
 
     def get_htdocs_dirs(self):
-        from pkg_resources import resource_filename
-
-        return [('trac', resource_filename(__name__, 'htdocs'))]
+        pass
